7266e5f73d1dbc3122c430f78f7b0e1b0f5950c7
Takahiro Yano <speg03@gmail.com>
2015-09-13 17:59:34 +0900

    Add proxy configurations

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,7 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "bento/centos-7.1"
+  config.vm.hostname = "webproxy.internal"
   config.vm.network "private_network", ip: "192.168.133.100"
   config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -18,3 +18,38 @@ firewall-cmd --set-default-zone internal
 ##   firewall-cmd --reload
 ## use nmcli instead of firewall-cmd
 nmcli connection modify enp0s3 connection.zone external
+
+# Transparent Proxy
+yum install -y squid
+cat <<EOF >/etc/squid/squid.conf
+acl localnet src 192.168.133.0/24
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 443
+acl CONNECT method CONNECT
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
+http_access allow localnet
+http_access allow localhost
+http_access deny all
+http_port 3128 transparent
+coredump_dir /var/spool/squid
+refresh_pattern . 0 20% 4320
+visible_hostname webproxy.internal
+EOF
+
+systemctl enable squid
+systemctl start squid
+
+setsebool -P squid_use_tproxy on
+
+firewall-cmd --permanent --zone=internal --add-port=3128/tcp
+firewall-cmd --permanent \
+             --direct --add-rule ipv4 nat PREROUTING 0 \
+             -i enp0s8 -p tcp --dport 80 \
+             -j REDIRECT --to-port 3128
+firewall-cmd --permanent \
+             --direct --add-rule ipv4 nat PREROUTING 0 \
+             -i enp0s8 -p tcp --dport 443 \
+             -j REDIRECT --to-port 3128
+firewall-cmd --reload
